fe851f24feb50aebe160268ac12bccef46211207
Amit <amitkulkarni2028@gmail.com>
2022-07-21 22:20:05 +0530

    Add test script

---
+++ test-appimages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if essential AppImages files have been bundled correctly
+function check_files() {
+	[ -s AppDir.desktop ] && echo "[OK] Desktop file exists" || exit 1
+	[ -s icon.png ] && echo "[OK] Icon file exists" || exit 1
+}
+
+function run_flint_test_binaries() {
+	for f in usr/bin/*; do
+		"$f" &>/dev/null && echo "[OK] $f" || echo "[ERR] $f"
+	done
+}
+
+check_files
+run_flint_test_binaries
