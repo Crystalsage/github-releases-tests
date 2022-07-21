ebcd7db83d23170cfee57b2c09ff2d71399197ec
Amit <amitkulkarni2028@gmail.com>
2022-07-21 23:19:44 +0530

    Update workflow

--- test-appimages.sh
+++ test-appimages.sh
@@ -1,7 +1,7 @@
 # Check if essential AppImages files have been bundled correctly
 function check_files() {
-	[ -s AppDir.desktop ] && echo "[OK] Desktop file exists" || exit 1
-	[ -s icon.png ] && echo "[OK] Icon file exists" || exit 1
+	[ -s AppDir.desktop ] && echo "[OK] Desktop file exists" || echo "[ERR] Desktop file does not exist"
+	[ -s icon.png ] && echo "[OK] Icon file exists" || echo "[ERR] Icon file does not exist"
 }
 
 function run_flint_test_binaries() {
